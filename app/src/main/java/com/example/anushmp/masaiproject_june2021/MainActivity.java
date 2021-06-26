43823fd028c1e0bc8791105afd2f5f20c43e4dfc
ampmasai <02.anush@gmail.com>
2021-06-26 13:28:56 +0530

    initial commit anush

--- app/src/main/java/com/example/anushmp/masaiproject_june2021/MainActivity.java
+++ app/src/main/java/com/example/anushmp/masaiproject_june2021/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //changed this.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
